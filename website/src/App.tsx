731bb899b2daff2701e097b7f7f8d262e91f1acb
Matej Koncal <matej.koncal@gmail.com>
2025-06-04 07:26:34 +0200

    Change page not found to _redirects

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,16 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MyPortfolio from './components/MyPortfolio';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MyPortfolio />} />
-        {/* Redirect all unknown routes to home */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <MyPortfolio />;
 }
 
 export default App;
